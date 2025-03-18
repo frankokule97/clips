a1a4dbf70444acbdd375e6ccafd0486e0f59827b
Franko Kulešević <franko@Frankos-MacBook-Pro-2.local>
2025-03-18 18:50:09 +0100

    fix: removed file that causes leak

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,10 +1,1 @@
-export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "AIzaSyDwUIEi8jgwQ-EXMLoVl9SS_AZquXYqWAY",
-    authDomain: "clips-ae550.firebaseapp.com",
-    projectId: "clips-ae550",
-    storageBucket: "clips-ae550.firebasestorage.app",
-    appId: "1:185793452675:web:6a51ca0ca0f8e0d62299b6"
-  }
-};
+
